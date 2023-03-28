e83376a04957da90a0e08c90844af4c529bd3fcd
paulohorizonti <pnagi2012@gmail.com>
2023-03-28 01:07:40 -0300

    COMMIT DO PROJETO PARKING-CONTROL

--- parking-control/src/main/java/com/api/parkingcontrol/Precisomtx1Application.java
+++ parking-control/src/main/java/com/api/parkingcontrol/Precisomtx1Application.java
@@ -2,12 +2,23 @@ package com.api.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Precisomtx1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Precisomtx1Application.class, args);
 	}
+	
+	@GetMapping("/")
+	public String index() {
+	
+			return "<h1>Olá Mundo</h1>";
+	
+	}
+	}
 
-}
+
